b6460db84c31acb7525756210b9071832aa60de6
Sergio Martin <smartinsantos@gmail.com>
2017-02-17 10:41:08 -0600

    added first component

---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nf-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent { }
