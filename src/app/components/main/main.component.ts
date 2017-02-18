403a171e0a4ecde194e1c1c7c044b525a45cca76
Sergio Martin <smartinsantos@gmail.com>
2017-02-18 09:54:34 -0600

    working on filter

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,3 +1,4 @@
+const _ = require('underscore')
 import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http'
 import { MovieService } from '../../services/movie.service'
@@ -18,6 +19,48 @@ export class MainComponent implements OnInit {
   fetchMovies() {
     this.movieService.getAll()
     .then(res => this.movies = !res.error ? res.data : this.movies)
+  }
+  searchFilter(movies, filterValue) {
+     if (filterValue.length < 1) { return movies }
+  let filter = filterValue.split(/[ ]+/)
+  let filterWordCount = filter.length
+  let containsString = (stringProp, word) => {
+    return stringProp.indexOf(word) >= 0
+  }
+  let filtered = []
+  // iterate all movies passed in
+  _.each(movies, (movie) => {
+    // iterate all the words passed in to the filter
+    let wordMatch = []
+    _.each(filter, (word) => {
+      let lcWord = word.toLowerCase()
+      let propMatch = false
+      // title check
+      let title = containsString(movie.title.toLowerCase(), lcWord)
+      if (title) { propMatch = propMatch || true }
+      // year check
+      let year = containsString(movie.year.toLowerCase(), lcWord)
+      if (year) { propMatch = propMatch || true }
+      // genre check
+      let genre = containsString(movie.genre.toLowerCase(), lcWord)
+      if (genre) { propMatch = propMatch || true }
+      // actor check
+      let actors = containsString(movie.actors.toLowerCase(), lcWord)
+      if (actors) { propMatch = propMatch || true }
+
+      if (propMatch) { wordMatch.push([movie, propMatch, word]) }
+    })
+    // if the number of word matches are the same as the number of words in the filter then check if the movie can be pushed
+    if (wordMatch.length === filterWordCount) {
+      _.each(wordMatch, (tern) => {
+        // wordMatch is an array of arrays [tern0,tern1] where tern is [movie,match,word]
+        if (tern[1] && !_.contains(filtered, tern[0])) {
+          filtered.push(tern[0])
+        }
+      })
+    }
+  })
+  return filtered
   }
   removeFromList(movieId) {
     this.movies.forEach((movie, index) => {
